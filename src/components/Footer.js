9fcbf84f2f628fd125a1069350fcce148c3f655e
kestertan <kestert@andrew.cmu.edu>
2023-02-25 15:43:07 -0500

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,119 +1,54 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import {
-  FaFacebookF,
-  FaGithub,
-  FaInstagram,
-  FaLinkedinIn
-} from "react-icons/fa";
-import LogoGray from "../images/btg-logo-gray.svg";
-import MailchimpForm from "../components/MailchimpForm";
+import React from 'react';
 
-
-
-const SocialMedia = () => {
+function Footer () {
+  const footerStyles = {
+    footer : {
+      position: "absolute",
+      bottom: "0",
+      width: "auto",
+      height: "5rem",
+      left: "5rem",
+      right: "5rem",
+    },
+    box : {
+      backgroundColor: "#D9D9D9",
+      height: "3rem",
+      width: "3rem",
+    },
+    textContainer : {
+      display: "flex",
+      justifyContent: 'space-between',
+      flexDirection : "column",
+      gap: "20",
+    },
+    text : {
+      fontFamily: "lato",
+      fontSize: '15px',
+      color: "#797979",
+    },
+    container: {
+      display: "flex",
+      justifyContent: 'space-between',
+      left: "20px",
+    }
+  }
   return (
-    <Col>
-      <Button
-        variant="light"
-        href="https://facebook.com/cmubtg"
-        target="_blank"
-        className="mr-1"
-      >
-        <FaFacebookF />
-      </Button>
-      <Button
-        variant="light"
-        href="https://instagram.com/cmubtg"
-        target="_blank"
-        className="mr-1"
-      >
-        <FaInstagram />
-      </Button>
-      <Button
-        variant="light"
-        href="https://github.com/cmubtg"
-        target="_blank"
-        className="mr-1"
-      >
-        <FaGithub />
-      </Button>
-      <Button
-        variant="light"
-        href="https://linkedin.com/company/cmubtg"
-        target="_blank"
-        className="mr-1"
-      >
-        <FaLinkedinIn />
-      </Button>
-    </Col>
+    <footer style={footerStyles.footer}>
+      <div style={footerStyles.container}>
+        <div style={footerStyles.textContainer}>
+          <div>
+            <span style={footerStyles.text}>Created by Carnegie Mellon University Business Technology Group</span>
+          </div>
+          <div>
+            <span style={footerStyles.text}>Contact </span>
+            <a style={footerStyles.text} href="https://cmubtg.com/">@info@cmu.btg.com</a>
+            <span style={footerStyles.text}> for inquires and concerns</span>
+          </div>
+        </div>
+        <div style={footerStyles.box}></div>
+      </div>
+    </footer>
   )
 }
-
-const LeftFooter = () => {
-  return (
-    <Container>
-      <Row className="text-secondary mt-2">
-        <small>
-          Have any questions? Feel free to reach out at {" "}
-          <a href="mailto:info@cmubtg.com" target="_blank" rel="noopener noreferrer">info@cmubtg.com</a>
-        </small>        
-      </Row>   
-      <Row className="mt-2">
-        <a href="https://github.com/cmubtg/btg-website/blob/master/CODE_OF_CONDUCT.md"
-          target="_blank" 
-          rel="noreferrer"
-          class="link-secondary"
-        >
-          <small>Code of Conduct</small>          
-        </a>
-      </Row>
-      <Row className="text-secondary mt-2">
-        <small>Deployed on {" "}
-          <a href="https://www.netlify.com/"
-            target="_blank" 
-            rel="noreferrer"
-            class="link-secondary"
-          >
-            <b>Netlify</b>
-          </a>
-          
-          </small>          
-        
-      </Row>      
-      <Row className="mt-3">
-        <SocialMedia />
-      </Row>   
-    </Container>
-  )
-}
-
-const Logo = () => {
-  return (
-    <Row className="mt-4">
-      <Col className="text-center">
-        <img src={LogoGray} alt="" width="50" height="50" />
-      </Col>
-  </Row>   
-  )
-}
-const Footer = () => {
-  return (
-    <div className="mt-5 border-top bg-white">
-      <Container className="footer" id="contact">
-        <Row>
-          <Col>
-            <LeftFooter />
-          </Col>
-        </Row>
-        <Logo />
-      </Container>
-    </div>
-
-  );
-};
 
 export default Footer;
