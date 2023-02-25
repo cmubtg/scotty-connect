874aa131f0e69d5f0c60161b166aab8d5e4a30a0
kestertan <kestert@andrew.cmu.edu>
2023-02-25 17:04:48 -0500

    Logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo from '../images/btg-logo-white-red-footer.svg';
 
 function Footer () {
   const footerStyles = {
@@ -10,10 +11,16 @@ function Footer () {
       left: "2.5rem",
       right: "2.5rem",
     },
+    divider: {
+      display: "absolute",
+      width: "100%",
+      bottom: "0",
+      border: "1px solid grey",
+    },
     box : {
-      backgroundColor: "#D9D9D9",
       height: "3rem",
       width: "3rem",
+      borderWidth: '0',
     },
     textContainer : {
       display: "flex",
@@ -26,6 +33,11 @@ function Footer () {
       fontSize: '15px',
       color: "#797979",
     },
+    text1 : {
+      fontFamily: "lato",
+      fontSize: '15px',
+      color: "#EE5C5C",
+    },
     container: {
       display: "flex",
       justifyContent: 'space-between',
@@ -33,21 +45,24 @@ function Footer () {
     }
   }
   return (
-    <footer style={footerStyles.footer}>
-      <div style={footerStyles.container}>
-        <div style={footerStyles.textContainer}>
-          <div>
-            <span style={footerStyles.text}>Created by Carnegie Mellon University Business Technology Group</span>
+    <div>
+      {/*<div style={footerStyles.divider}></div>*/}
+      <footer style={footerStyles.footer}>
+        <div style={footerStyles.container}>
+          <div style={footerStyles.textContainer}>
+            <div>
+              <span style={footerStyles.text}>Created by Carnegie Mellon University Business Technology Group</span>
+            </div>
+            <div>
+              <span style={footerStyles.text}>Contact </span>
+              <a style={footerStyles.text1} href="mailto:info@cmubtg.com">@info@cmu.btg.com</a>
+              <span style={footerStyles.text}> for inquires and concerns</span>
+            </div>
           </div>
-          <div>
-            <span style={footerStyles.text}>Contact </span>
-            <a style={footerStyles.text} href="mailto:info@cmubtg.com">@info@cmu.btg.com</a>
-            <span style={footerStyles.text}> for inquires and concerns</span>
-          </div>
+          <img src={Logo} alt={Logo} style={footerStyles.box} />
         </div>
-        <div style={footerStyles.box}></div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   )
 }
 
