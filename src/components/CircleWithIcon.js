dbee5f91ec7def595ce8180cd2abb860546ef7eb
Sharon Li <sharon@sharons-mbp-5.wifi.local.cmu.edu>
2023-04-09 20:52:40 -0400

    imported lato font to styles css and added modal of info icon to main page

--- src/components/CircleWithIcon.js
+++ src/components/CircleWithIcon.js
@@ -5,10 +5,6 @@ function CircleWithIcon() {
   const [showModal, setShowModal] = useState(false);
   const modalRef = useRef();
 
-  const handleClick = () => {
-    setShowModal(!showModal);
-  };
-
   const handleModalClose = (e) => {
     if (modalRef.current && !modalRef.current.contains(e.target)) {
       setShowModal(false);
@@ -22,26 +18,24 @@ function CircleWithIcon() {
     };
   }, []);
 
-  const triangle = showModal ? <div className="triangle"></div> : null;
-
   return (
-    <div className="circle" onClick={handleClick}>
+    <div className="circle" onMouseEnter={() => setShowModal(true)} onMouseLeave={() => setShowModal(false)}>
       <div className="icon">
         <div className="small-circle"></div>
-        <div className="rectangle">{triangle}</div>
-        {/* <div className="rectangle">{triangle}</div> */}
+        <div className="rectangle">{showModal && <div className="triangle"></div>}</div>
       </div>
       {showModal && (
         <div ref={modalRef} className="modal-overlay">
-          {/* <div className="modal"> */}
-            <h2><b>Welcome to ScottyConnect! 👋</b></h2>
-            <h3>We help you navigate your academic path by providing a list of 
-              courses Carnegie Mellon students have taken in the past. </h3>
-            <h3>You can search by majors or include additional filters to find 
-              people that are most similar to you. We hope you find our website helpful!</h3>
-            <p>Scotty Connect is a platform created by Carnegie Mellon’s 
-              Business Technology Group.</p>
-          {/* </div> */}
+          <h2><b>Welcome to ScottyConnect! 👋</b></h2>
+          <h2> </h2>
+          <h3>We help you navigate your academic path by providing a list of 
+            courses Carnegie Mellon students have taken in the past. </h3>
+          <h2> </h2>
+          <h3>You can search by majors or include additional filters to find 
+            people that are most similar to you. We hope you find our website helpful!</h3>
+          <h2> </h2>
+          <p>Scotty Connect is a platform created by Carnegie Mellon’s 
+          Business Technology Group.</p>
         </div>
       )}
     </div>
