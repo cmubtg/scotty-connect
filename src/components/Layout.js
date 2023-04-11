e324d044147bf0e27a62822d87b8ccedbd6e1cce
Sharon Li <sharon@Sharons-MacBook-Pro-5.local>
2023-04-11 04:30:26 -0400

    added saved button and directed it to saved page but need to fix positioning of saved

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/styles.css'
+import '../css/styles.css';
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import FadeIn from "react-fade-in";
+import { Link } from 'gatsby';
 
-export default function Layout({ children }) {
-  
+export default function Layout({ children, showSavedText = false }) {
   return (
     <div style={{minHeight: "100vh", position: "relative"}}>
       <FadeIn>
@@ -14,6 +14,11 @@ export default function Layout({ children }) {
         <div style={{paddingBottom: "5rem"}}>
           {children}
         </div>
+        {showSavedText && (
+          <Link to="/saved" className="saved-text" style={{ cursor: "pointer", textDecoration: "none", color: "white" }}>
+            Saved
+          </Link>
+        )}
         <Footer/>
       </FadeIn>
     </div>
