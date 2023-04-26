2dd1a982be072ec388803ed4e4f56437c9de0d7d
Ire Alarape <ialarape@andrew.cmu.edu>
2023-04-26 12:14:36 -0400

    Improved sorting function

--- src/pages/results.js
+++ src/pages/results.js
@@ -29,10 +29,38 @@ function Results() {
     navigate('/profile')
   }
 
-  const selectedStudents = SearchData.filter(studentInfo => hasAllElems(studentInfo.major, get_labels(selectedMajors)))
-  console.log("selected",selectedStudents)
-  console.log("selected majors",selectedMajors)
-  console.log("all",allMajors)
+  
+  // Gets how closely a student's majors match the search
+  function getScore(studentMajors, selectedMajors){
+    var score = 0
+    for (var i = 0; i < studentMajors.length; i++) {
+      var major = studentMajors[i]
+      if (selectedMajors.includes(major)){
+        score += 1
+      }
+    }
+    return score
+  }
+
+  // Sorting function
+  function compareStudentScore(a,b){
+    return getScore(a) - getScore(b)
+  }
+
+  // Gets students who match selected majors
+  // Orders it based on score (how closely their majors match the search)
+  function selectStudents(){
+    var selectedStudents = []
+    for (var i = 0; i < SearchData.length; i++) {
+      var studentInfo = SearchData[i]
+      var score = getScore(studentInfo.major,get_labels(selectedMajors))
+      // Dont include the students that dont match at all
+      if (score > 0) selectedStudents.push(studentInfo)
+    }
+    return selectedStudents.sort(compareStudentScore)
+  }
+  
+  const selectedStudents = selectStudents();
   return (
     <Layout>
       <Container className="mt-md-1 pt-md-4">
